392c7ec70e16ef2b1612dab0314cb1fb30ee8d61
patnirishabh2911 <rishabh.p@toppr.com>
2021-05-30 11:03:06 +0530

    Initial commit

---
+++ src/main/java/RestaurantService.java
@@ -0,0 +1,40 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+
+    public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException {
+
+        Optional<Restaurant> optionalRestaurant = restaurants.stream()
+                .filter(restaurant ->
+                        restaurantName.equals(restaurant.getName()))
+                .findFirst();
+
+        if (optionalRestaurant.isPresent()) {
+            return optionalRestaurant.get();
+        } else {
+            throw new restaurantNotFoundException(restaurantName);
+        }
+
+    }
+
+
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+
+    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+}
